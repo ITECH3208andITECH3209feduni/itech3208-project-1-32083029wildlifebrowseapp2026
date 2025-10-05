e87524148def92cbce549b917b872198a9ba844e
divi-divi-divi <dm2@students.federation.edu.au>
2025-10-06 00:29:54 +1100

    Resolved merge conflict on landowner-registration (landowner page won't be accessible)

--- flutter_Base_Project/lib/landowner-view/landowner_registration.dart
+++ flutter_Base_Project/lib/landowner-view/landowner_registration.dart
@@ -207,14 +207,9 @@ class _LandownerFormTabs extends State<LandownerFormTabs> with SingleTickerProvi
         context,
         rootNavigator: true,
       ).pushNamed(
-<<<<<<< HEAD
         '/request-board',
         arguments: {'user': user},
-=======
-        '/landowner',
-        arguments: {'user': user, 'uploadSuccess': true},
->>>>>>> 1b5f75082a1e6ed823f9c061e245f3b15d9aa265
-        );
+      );
     }
     else{
       showDialog(
